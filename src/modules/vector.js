acc74efc37992c1e4f638c03828004e91fe24e02
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-09-21 12:46:37 +1000

    Create vector class

---
+++ src/modules/vector.js
@@ -0,0 +1,21 @@
+export default class Vector {
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x = null, y = null) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Calculate distance between origin of this vector and point on x and y cooridnates. Will be used to calculate hit position on a ship object.
+     * @param {number} x 
+     * @param {number} y 
+     * @returns {number}
+     */
+    calculateDistance(x, y) {
+        return Math.sqrt(Math.abs(y - this.y) ^ 2 + Math.abs(x - this.x) ^ 2);
+    }
+}
