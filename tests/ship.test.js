7cbbf76764a853144a52f5cd25c8f90a54507607
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-09-22 11:33:51 +1000

    Update Ship class tests

--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,27 +1,24 @@
 import Ship from "../src/modules/ship"
-import Vector from "../src/modules/vector"
 
 describe('Feature: Ship class', () => {
-    describe('When accessing ship origin with getOrigin()', () => {
-        test('Then it\'s should return origin value', () => {
-            expect(new Ship('ship', new Vector(2, 3), 3).getOrigin()).toStrictEqual(new Vector(2, 3))
-        })
-    })
-
-    describe('When hit a 1 length ship in position 1', () => {
-        test('Then it\'s should not be sunk if isSunk() is called', () => {
-            const ship = new Ship('smol dude', new Vector(0, 0), 1);
-            ship.hit(1);
-            expect(ship.isSunk()).toBe(false);
-        })
-    })
-
-    describe('When ship with length of 2 got hit two times', () => {
+    describe('When a ship with length 1 got hit 1 time', () => {
         test('Then it\'s should be sunk', () => {
-            const ship = new Ship('ship', new Vector(0, 0), 2);
-            ship.hit(1);
-            ship.hit(0);
-            expect(ship.isSunk()).toBe(true);
+            const s = new Ship('small', 1);
+            s.hit();
+            expect(s.isSunk()).toBe(true);
+        })
+    })
+
+    describe('When a ship with length 2 got hit', () => {
+        const s = new Ship('ship', 2);
+        test('Then after one hit it is not sunk', () => {
+            s.hit();
+            expect(s.isSunk()).toBe(false);
+        })
+
+        test('Then after the second hit it is sunk', () => {
+            s.hit();
+            expect(s.isSunk()).toBe(true);
         })
     })
 })
