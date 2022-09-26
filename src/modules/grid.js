c78967e11afca60f64cb3623b39b30cc1126b981
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-09-26 10:07:31 +1000

    Fix Grid constructor loop

--- src/modules/grid.js
+++ src/modules/grid.js
@@ -3,9 +3,9 @@ export default class Grid {
 
     constructor(size = 8, defaultElement = null) {
         this.#array.length = 0;
-        for (let i = 0; i <= size; i++) {
+        for (let i = 0; i < size; i++) {
             this.#array.push([]);
-            for (let j = 0; j <= size; j++)
+            for (let j = 0; j < size; j++)
                 this.#array[i].push(defaultElement);
         }
     }
