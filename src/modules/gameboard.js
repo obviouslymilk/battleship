f5bf64759724495a45ebe415575ee1c52356bf8d
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-09-26 11:37:34 +1000

    Add addShip() function to Gameboard

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -13,11 +13,22 @@ export default class Gameboard {
     }
 
     addShip(ship, x, y, rotate) {
-        if (!ship instanceof Ship)
+        if (!(ship instanceof Ship))
             throw new Error(`addShip() expected instance of Ship, got ${ship.constructor.name}`);
-        
-        if ( x > Gameboard.#GRID_SIZE - (rotate ? 1 : ship.getLength()) || y > Gameboard.#GRID_SIZE - (rotate ? ship.getLength() : 1))
+        if ( x > Gameboard.#GRID_SIZE - (rotate ? 1 : ship.getLength()) || y > Gameboard.#GRID_SIZE - (rotate ? ship.getLength() : 1) || x < 0 || y < 0)
             throw new Error('addShip() coordinates are out of bounds.');
+        
+        let isOccupied = false;
+        for (let i = 0; i < ship.getLength(); i++)
+            if (this.#grid.getData(rotate ? x : x + i, rotate ? y + i : y).ship)
+                isOccupied = true;
+        if (isOccupied)
+            throw new Error('addShip() cannot add a ship on occupied space(s).');
+        
+        this.#ships.push(ship);
+
+        for (let i = 0; i < ship.getLength(); i++)
+            this.#grid.setData(rotate ? x : x + i, rotate ? y + i : y, { ship: ship, marked: false });
     }
 
     getShipInCell(x, y) {
