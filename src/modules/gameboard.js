a5cee87b338cbf59fb84cfe60a96e683aa12d661
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-09-26 10:19:57 +1000

    Create Gameboard class

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,8 +1,30 @@
+import Grid from './grid';
 import Ship from './ship';
 
 export default class Gameboard {
 
-    constructor() {
+    static #GRID_SIZE = 8;
 
+    #ships = [];
+    #grid;
+
+    constructor() {
+        this.#grid = new Grid(Gameboard.#GRID_SIZE, { ship: null, marked: false });
+    }
+
+    addShip(ship, x, y, rotate) {
+        if (!ship instanceof Ship)
+            throw new Error(`addShip() expected instance of Ship, got ${ship.constructor.name}`);
+        
+        if ( x > Gameboard.#GRID_SIZE - (rotate ? 1 : ship.getLength()) || y > Gameboard.#GRID_SIZE - (rotate ? ship.getLength() : 1))
+            throw new Error('addShip() coordinates are out of bounds.');
+    }
+
+    getShipInCell(x, y) {
+        return this.#grid.getData(x, y).ship;
+    }
+
+    isCellMarked(x, y) {
+        return this.#grid.getData(x, y).marked;
     }
 }
