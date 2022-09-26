ffbf156cb99f40cd80d0af7b30d28258a4058425
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-09-26 11:58:50 +1000

    Refactor Grid tests

--- tests/grid.test.js
+++ tests/grid.test.js
@@ -3,16 +3,16 @@ import Grid from "../src/modules/grid";
 describe('Feature: Grid', () => {
     describe('When create Grid(8, 0) and add 1 to [3;3]', () => {
         const grid = new Grid(8, 0);
-        grid.setData(3, 3, 1)
+        grid.setMarked(3, 3)
         test('Then getData(3, 3) should return 1', () => {
-            expect(grid.getData(3, 3)).toBe(1);
+            expect(grid.getData(3, 3).marked).toBe(true);
         })
 
-        test('Then getData(0, 0) should return 0', () => {
-            expect(grid.getData(0, 0)).toBe(0);
+        test('Then getData(0, 0) should return {ship: null, marked: null}', () => {
+            expect(grid.getData(0, 0)).toStrictEqual({ship: null, marked: null});
         })
 
-        test('Then setData(8, 8) should throw', () => {
+        test('Then setData(8, 8, 1) should throw', () => {
             expect(() => { grid.setData(8, 8, 1) }).toThrow();
         })
     })
