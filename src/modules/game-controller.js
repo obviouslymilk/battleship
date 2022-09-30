e3cf0c424b47f6c4431724aa6d2545c4cc698420
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-09-30 10:32:33 +1000

    Add JSdocs

--- src/modules/game-controller.js
+++ src/modules/game-controller.js
@@ -19,15 +19,27 @@ export default class GameController {
         // add ships to ai
     }
 
+    /**
+     * Switch players.
+     */
     #switchPlayer() {
         this.#currentPlayer = this.#currentPlayer === this.#players[0] ? this.#players[1] : this.#players[0];
     }
 
+    /**
+     * Print player's information and gameboard in the console.
+     * @param {Object} player Player object to print.
+     * @param {boolean} hide Should ships be hidden or not.
+     */
     #printPlayer(player, hide = false) {
         console.log(`${player.name.toUpperCase()}'s BOARD.`);
         player.gameboard.print(hide);
     }
 
+    /**
+     * Returns current player.
+     * @returns {Object} Current player object.
+     */
     getCurrentPlayer() {
         return this.#currentPlayer;
     }
